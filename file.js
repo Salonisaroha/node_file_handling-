5365c26396cd7bc25865bcbd6138cca135c55020
Saloni <115524019+Salonisaroha@users.noreply.github.com>
2023-10-21 19:43:31 +0530

    __CRUD__

---
+++ file.js
@@ -0,0 +1,42 @@
+// read file.
+
+const fs = require('fs');
+// fs.readFile('./blog.txt',(err, data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+// write file.
+// fs.writeFile('./blog.txt2','hello world',()=>{
+// console.log("file was written");
+// });
+
+// directories
+// if(!fs.existsSync('./assets')){
+//     fs.mkdir('./assets', (err)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log('folder created')
+//     });
+// } else{
+//     fs.rmdir('./assets', (err)=>{
+//         if (err){
+//             console.log(err)
+//         }
+//         console.log('folder created')
+//     })
+// }
+
+// deleting files
+// if(fs.existsSync('./delete.txt')){
+//     fs.unlink('./delete.txt',(err)=>{
+//         if(err){
+//             console.log(err)
+//         }
+//         console.log('file deleted')
+//     })
+// }
+
+
